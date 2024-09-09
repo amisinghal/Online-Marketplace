0fc1b5f588936ca8655ff565c9a1fe917dbcd9b3
amisha singhal <42436978+amisinghal@users.noreply.github.com>
2024-09-08 23:41:53 -0400

    Front page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
 # Create your views here.
 # request= info about the browswer, ip address and the api request type get or post
 # loading the templates/core here, templates are automatically located by django in the installed apps under settigns.py
 def index(request):
-    return render(request, 'core/index.html')
+    items= Item.objects.filter(is_sold=False)[0:6]
+    categories= Category.objects.all()
+    return render(request, 'core/index.html', {'categories':categories, 'items': items})
 
 def contact(request):
     return render(request, 'core/contact.html')
